d0295954a51b303550843dd10a438c4aedd1ad89
fujikitoshiki <fujiki@example.com>
2020-10-24 21:47:00 +0900

    lesson02

--- config/mamazon/views.py
+++ config/mamazon/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
+class Home(TemplateView):
+    template_name = 'mamazon/home.html'
+    
 # Create your views here.
