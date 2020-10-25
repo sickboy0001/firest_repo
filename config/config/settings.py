c3cbfdfa2e47c5c48deb277a6d5efb65f2fc1556
fujikitoshiki <fujiki@example.com>
2020-10-26 06:34:15 +0900

    mamazon lesson4

--- config/config/settings.py
+++ config/config/settings.py
@@ -121,3 +121,6 @@ USE_TZ = True
 import os
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR , 'static')]
+
+MEDIA_URL ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
--- config/mamazon/models.py
+++ config/mamazon/models.py
@@ -7,3 +7,6 @@ class Product(models.Model):
   price = models.IntegerField()
   description = models.TextField()
   thumbnail = models.ImageField(upload_to='thumbnails/')
+
+  def __str__(self):
+    return self.name
