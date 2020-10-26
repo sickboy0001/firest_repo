63f7e54f75850092a48b8287396b3517ccae349b
fujikitoshiki <fujiki@example.com>
2020-10-27 07:03:49 +0900

    lesson08

--- config/cart/models.py
+++ config/cart/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from django.conf import settings
 from mamazon.models import Product
 
-User=settings.AUTO_USER_MODEL
+User = settings.AUTH_USER_MODEL
 
-class cart(models.Model):
+
+class Cart(models.Model):
   user = models.ForeignKey(User,null=True,blank=True,on_delete=models.CASCADE)
-  products = models.ManyToManyField(Product,blank=true)
+  products = models.ManyToManyField(Product,blank=True)
   total = models.DecimalField(default=0.00 , max_digits=9 , decimal_places=2) #100,000,000.00
-  created = models.DateTimeField(auto_now_add=true)
-  updated = models.DateTimeField(auto_now=true)
-  
+  created = models.DateTimeField(auto_now_add=True)
+  updated = models.DateTimeField(auto_now=True)
